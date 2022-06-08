0658f2010b1d0af8cce93195bbe5d66c12d7dabe
vuquangngoc <vuquangngocnamdinh@gmail.com>
2022-06-08 16:21:41 +0700

    .

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:quizstar/quizpage.dart';
 // import 'package:device_preview/device_preview.dart';
 //import 'package:audioplayers/audioplayers.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:unicorndial/unicorndial.dart';
 class homepage extends StatefulWidget {
 
   @override
@@ -19,13 +22,14 @@ class _homepageState extends State<homepage> {
     "images/sinhhoc.png",
   ];
 
-  List<String> des = [
-    "Để tăng tốc khả năng tính toán , hãy chuẩn bị sẵn máy tính và giấy bút cùng một cái đầu lạnh.\nLưu ý: Mỗi câu hỏi chỉ có 60 giây nên hãy cố gắng !",
+ List<String> des = [
+    "Toán học là môn học áp dụng nhiều trong đời sống\nĐể làm tốt một bài toán, hãy chuẩn bị cho mình máy tính bỏ túi và giấy bút cùng một cái đầu lạnh!",
     "Vật lý là một môn học không khó mà cũng không dễ dàng\nHãy nắm chắc kiến thức trong đầu và máy tính CASIO trên tay để bước vào bài thi",
     "Hóa học là môn học đòi hỏi tư duy cao và phản xạ nhanh, linh hoạt.\n Hãy chuẩn bị sẵn combo bảng tuần hoàn + máy tính + giấy nháp để sẵn sàng bước vào những câu hỏi đầu tiên nào!",
     "Tiếng Anh là ngôn ngữ phổ biến nhất thế giới nên việc học Tiếng Anh là rất cần thiết cho tương lai mỗi người",
     "Sinh học là môn học khá khó học với một số bạn nhưng nếu các bạn tìm hiểu và mầy mò học hỏi tìm tòi thì có thế các bạn sẽ yêu môn học này đó",
   ];
+
 
 
 
@@ -118,6 +122,75 @@ class _homepageState extends State<homepage> {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown, DeviceOrientation.portraitUp
     ]);
+var childButtons = List<UnicornButton>();
+
+    childButtons.add(UnicornButton(
+        hasLabel: true,
+        labelText: "Lưu ý",
+        currentButton: FloatingActionButton(
+          heroTag: "noti",
+          backgroundColor: Colors.green,
+          mini: true,
+          child: Icon(Icons.announcement_outlined),
+          onPressed: () {
+            showAlertDialog(context);
+          },
+        )));
+childButtons.add(UnicornButton(
+        labelText: "Facebook",
+        hasLabel: true,
+        currentButton: FloatingActionButton(
+            heroTag: "facebook",
+            backgroundColor: Colors.blueAccent,
+            mini: true,
+            child: Icon(Icons.facebook),
+          onPressed: () async {
+            String url5 = "https://m.me/vuquangngoc1309/";
+            var urllaunchable = await canLaunch(url5); //canLaunch is from url_launcher package
+            if(urllaunchable){
+              await launch(url5); //launch is from url_launcher package to launch URL
+            }else{
+              print("URL can't be launched.");
+            }
+          },
+        )));
+
+    childButtons.add(UnicornButton(
+        labelText: "Telegram",
+        hasLabel: true,
+        currentButton: FloatingActionButton(
+            heroTag: "telegram",
+            backgroundColor: Colors.lightBlue,
+            mini: true,
+            child: Icon(Icons.wifi_tethering_sharp),
+          onPressed: () async {
+            String url2 = "https://t.me/Vuquangngoc";
+            var urllaunchable = await canLaunch(url2); //canLaunch is from url_launcher package
+            if(urllaunchable){
+              await launch(url2); //launch is from url_launcher package to launch URL
+            }else{
+              print("URL can't be launched.");
+            }
+          },)));
+
+    childButtons.add(UnicornButton(
+        labelText: "Hotline",
+        hasLabel: true,
+        currentButton: FloatingActionButton(
+            heroTag: "call",
+            backgroundColor: Colors.purpleAccent,
+            mini: true,
+            child: Icon(Icons.phone_enabled),
+          onPressed: () async {
+            final String telephoneNumber = "0904892301";
+            String telephoneUrl = "tel:$telephoneNumber";
+            if (await canLaunch(telephoneUrl)) {
+              await launch(telephoneUrl);
+            } else {
+              throw "Can't phone that number.";
+            }
+          },
+        )));
     return Scaffold(
 
       // appBar: AppBar(
@@ -253,13 +326,23 @@ class _homepageState extends State<homepage> {
     // thiết kế AlertDialog - bảng thông báo
     AlertDialog alert = AlertDialog(
       title: Text("Lưu ý:"),
-      content: Text("Mỗi lần thi sẽ bao gồm 20 câu hỏi .\nMỗi câu hỏi có 60 giây để chọn ứng với mỗi đáp án là 5 điểm.\nNếu hết thời gian thí sinh không chọn đáp án thì câu hỏi đó sẽ không tính điểm !",
-        style: TextStyle(
-          fontSize: 17,
-          // fontWeight: FontWeight.bold,
-          color: Colors.black,
-          fontFamily: "Roboto",
-        ),),
+      content: Text.rich(
+          TextSpan(
+              // style: TextStyle(fontSize: 27,),
+              children: [
+
+                TextSpan(
+                    text: "Mỗi lần thi sẽ bao gồm 20 câu hỏi .\nMỗi câu hỏi có 60 giây để chọn ứng với mỗi đáp án là 5 điểm.\nNếu hết thời gian thí sinh không chọn đáp án thì câu hỏi đó sẽ không tính điểm !\n\n",
+            style: TextStyle(
+              fontSize: 17,
+              // fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontFamily: "Roboto",
+            ),),
+        ]
+          )
+      ),
+
       actions: [
         okButton,
       ],
@@ -273,5 +356,12 @@ class _homepageState extends State<homepage> {
         return alert;
       },
     );
+  }
+}
+Future<void> _onOpen(LinkableElement link) async {
+  if (await canLaunch(link.url)) {
+    await launch(link.url);
+  } else {
+    throw 'Could not launch $link';
   }
 }
